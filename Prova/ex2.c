2b9d680e1149237c946c54ff0a60d92dd63bcb5a
Eridan <genealerooo@gmail.com>
2025-10-03 13:39:15 -0400

    FIX correção questao 2

--- Prova/ex2.c
+++ Prova/ex2.c
@@ -2,48 +2,47 @@
 #include <stdlib.h>
 #include <string.h>
 
-int* dobrar_mem(int** , size_t, size_t);
+int* dobrar_mem(int** , size_t, size_t tamanho_antigo);
 
-int* dobrar_mem(int **vetor_velho, size_t n, size_t j){
+int* dobrar_mem(int **vetor_velho, size_t n, size_t tamanho_antigo){
     int *novo_vetor;
-    size_t i;
-    n *= 2;
-
+    size_t i = 0;
+    
     novo_vetor = (int *) malloc(n * sizeof(int));
-
-    for (i = 0; i < j; i++){
-        novo_vetor[i] = **vetor_velho + i;
+    
+    for (i = 0; i < tamanho_antigo; i++){
+        *(novo_vetor + i) = *(*vetor_velho + i);
     }
-
-    return *&novo_vetor;
+    
+    free(*vetor_velho);
+    return novo_vetor;
 }
 
 int main(){
-    int i;
-    size_t n = 5, j = 0;
+    size_t index, n = 5, tamanho_antigo = 0, index2;
     int *vetor;
-
+    
     vetor = (int *) malloc(n * sizeof(int));
 
     while (1){
-
-        for (i = j; i < n; i++){
+        
+        for (index = tamanho_antigo; index < n; index++){
             printf("N = ");
-            scanf("%d",vetor + i);
-
-            if (*vetor + i * sizeof(int) == -1) n = 0; break;
+            scanf("%d",vetor + index);
+            if (*(vetor + index) == -1) break;
         }
-
-        if (n == 0) break;
-
-        if (i == n){
-            j = n;
-            vetor = (int*) dobrar_mem(&vetor, n, j);
+        
+        if (index == n){
+            tamanho_antigo = n;
+            n *= 2;
+            vetor = dobrar_mem(&vetor, n, tamanho_antigo);
+        } else{
+            break;
         }
     }
 
-    for (j = 0; j < i; j++){
-        printf("Posicao %d = %d", j, *vetor + j * sizeof(int));
+    for (index2 = 0; index2 < index; index2++){
+        printf("Posicao %d = %d\n", index2, *(vetor + index2));
     }
 
     free(vetor);
