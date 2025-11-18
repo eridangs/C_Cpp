d488169763d0ee5aedd4832dff70b71b01f2acd1
Eridan <genealerooo@gmail.com>
2025-11-18 18:15:47 -0400

    Feitaa

--- ATV 4 PILHA_OP/main.cpp
+++ ATV 4 PILHA_OP/main.cpp
@@ -1,34 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string>
+#include <string.h>
 
 // PILHA E CHAVES
 
-struct cel {
-    char chave;
-    struct cel *seg;
-};
+#define MAX 2005
 
-typedef struct cel celula;
-
-empilhar();
-desempilhar();
+void empilhar (char *pilha, int *t, char chave);
+void desempilhar (char *pilha, int *t);
 
 int main() {
-    celula c, *cabeca;
-
-    c.seg = NULL;
-    cabeca = &c;
-
-
-
-
-
-
-
-
-
-
+    char linha[MAX], *pilha;
+    int casos = 1, t = 0, i = 0;
+    pilha = (char*) malloc(MAX * sizeof(char));
     
+    while (scanf("%s", linha) != EOF) {
+        if (linha[0] == '-') {
+            break;
+        }
+
+        t = 0;
+        for (i = 0; linha[i] != '\0'; i++) {
+            char chave = linha[i];
+
+            if (t != 0 && pilha[t-1] == '{' && chave == '}') {
+                desempilhar(pilha, &t);
+            } else {
+                empilhar(pilha, &t, chave);
+            }
+        }
+        int abre = 0, fecha = 0;
+
+        for (t; 0 < t; t--) {
+            if (pilha[t-1] == '{') abre++;
+            else fecha++;
+        }
+
+        int trocas = (abre + 1) / 2 + (fecha + 1) /2 ;
+
+        printf("%d. %d\n",casos, trocas);
+        casos++;
+    }
+    
+    
+
     return 0;
+}
+
+void empilhar (char *pilha, int *t, char chave) {
+    if ((*t) != MAX) {
+        pilha[(*t)] = chave;
+        (*t)++;
+    }
+}
+void desempilhar (char *pilha, int *t) {
+    if ((*t) != 0) {
+
+        (*t)--;
+    }
 }
